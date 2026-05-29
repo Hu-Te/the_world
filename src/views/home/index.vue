06fb3b333ff10f6e1724df1d8b7fd1860ecefa67
Hu-Te <1261753654@qq.com>
2026-05-29 11:16:41 +0800

    初始化项目

---
+++ src/views/home/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="home">
+  </div>
+</template>
+
+<style scoped>
+.home {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  margin: 0;
+  color: #213547;
+}
+
+h1 {
+  margin: 0 0 0.5rem;
+  font-size: 2rem;
+  font-weight: 500;
+}
+
+p {
+  margin: 0;
+  color: #64748b;
+}
+</style>
